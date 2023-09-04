ea35d6d4fc06ab87cb29ae53ef2916ce48d6172c
Paula Marti <paula0173@hotmail.com>
2023-09-03 21:05:44 -0300

    Modificacion App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,61 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useState } from "react";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+import Buscador from "./components/Buscador";
+import Formulario from './components/Formulario';
+import Listado from './components/Listado';
+import Alerta from './components/Alerta';
+
+import { baseColaboradores } from "../src/Basecolaboradores.js"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUsers } from '@fortawesome/free-solid-svg-icons'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [listaColaboradores, setColaboradores] = useState(baseColaboradores);
+
+  const [listaBusqueda, setListaBusqueda] = useState(listaColaboradores);
+  const [estadoAlerta, setestadoAlerta] = useState({ mensaje: "", color: "" })
+
+  const agregarColaborador = (datosColaborador) => {
+
+    setColaboradores([...listaColaboradores, { ...datosColaborador }]);
+    setListaBusqueda([...listaColaboradores, { ...datosColaborador }]);
+
+  };
+
+  const filtrarDatos = (nomtexto) => {
+
+    const listaFiltrada = listaColaboradores.filter(colab => colab.nombre.toLowerCase().includes(nomtexto)
+      || colab.correo.toLowerCase().includes(nomtexto)
+      || colab.edad.toLowerCase().includes(nomtexto)
+      || colab.cargo.toLowerCase().includes(nomtexto)
+      || colab.telefono.toLowerCase().includes(nomtexto))
+    setListaBusqueda(listaFiltrada);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Container >
+        <Row>
+          <h3 className='mt-3'><FontAwesomeIcon icon={faUsers} />Lista de Colaboradores</h3>
+          <Col className="mt-2"  > <Buscador filtrarDatos={filtrarDatos} /> </Col>
+        </Row>
+
+        <Row className="d-flex mt-4 justify-content-center">
+          <Col sm={12} md={9} lg={8}> <Listado listaColaboradores={listaBusqueda} />  </Col>
+          <Col sm={12} md={3} lg={4}> 
+             
+                <Formulario estadoAlerta={setestadoAlerta} agregarColaborador={agregarColaborador} /> 
+                <Alerta className="mt-2" estadoAlerta={estadoAlerta} />      
+          </Col>
+        </Row>
+      </Container>
     </>
   )
 }
